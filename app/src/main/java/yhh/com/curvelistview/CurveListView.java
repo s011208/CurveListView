39a269cfb63ca11cd651ba122ae63df937b1a92f
Yen-Hsun, Huang <s011208@gmail.com>
2015-05-08 18:05:37 +0800

    add simple listview

---
+++ app/src/main/java/yhh/com/curvelistview/CurveListView.java
@@ -0,0 +1,7 @@
+package yhh.com.curvelistview;
+
+/**
+ * Created by Yen-Hsun_Huang on 2015/5/8.
+ */
+public class CurveListView {
+}
---
+++ app/src/main/java/yhh/com/curvelistview/RoundListAdapter.java
@@ -0,0 +1,67 @@
+package yhh.com.curvelistview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Yen-Hsun_Huang on 2015/5/8.
+ */
+public class RoundListAdapter extends BaseAdapter {
+    private final Context mContext;
+    private final LayoutInflater mInflater;
+    private static final int SIZE = 20;
+    private static final ArrayList<Integer> mItems = new ArrayList<Integer>();
+
+    static {
+        for (int i = 0; i < SIZE; i++) {
+            mItems.add(i);
+        }
+    }
+
+    public RoundListAdapter(Context context) {
+        mContext = context;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return SIZE;
+    }
+
+    @Override
+    public Integer getItem(int position) {
+        return mItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = mInflater.inflate(R.layout.item, null);
+            holder.mBg = (ImageView) convertView.findViewById(R.id.bg);
+            holder.mFg = (ImageView) convertView.findViewById(R.id.fg);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.mBg.setImageResource(R.drawable.bg);
+        holder.mFg.setImageResource(R.drawable.fg);
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        ImageView mBg, mFg;
+    }
+}
